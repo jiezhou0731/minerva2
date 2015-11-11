620a290b587ce6c8538ce79995e81093f1a0dc65
jiezhou0731 <jiezhou0731@gmail.com>
2015-11-11 14:41:52 -0500

    detection

--- client/app/data/electronic/crimeDetectioin.py
+++ client/app/data/electronic/crimeDetectioin.py
@@ -33,25 +33,91 @@ for fileNumber in range (0,20): #1727):
             #    continue
 
             # d is what will be transfered to solr system
-            if (type(doc["_source"]["timestamp"]) is list):
-                doc["_source"]['timestamp'] = doc["_source"]["timestamp"][0]
+            d={"cdr_data":""}
+            d['cdr_data'] = json.dumps(doc["_source"])
+            d['_index'] = str(doc["_index"])
+            d['_type'] = str(doc["_type"])
+            d['id'] = str(doc["_id"])
+            if (type(doc["_source"]["url"]) is list):
+                d['url'] = doc["_source"]["url"][0]
             else:
-                doc["_source"]['timestamp'] = doc["_source"]["timestamp"]
+                d['url'] = doc["_source"]["url"]
 
+            d['url'] = ''.join(d['url'])
+            d['url'] = d['url'].partition('?')[0]
+
+            if (type(doc["_source"]["timestamp"]) is list):
+                d['timestamp'] = doc["_source"]["timestamp"][0]
+            else:
+                d['timestamp'] = doc["_source"]["timestamp"]
+            url =  d['url']
+            html = unicode(doc["_source"]["raw_content"])
+            parsed_uri = urlparse( url )
+            domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+
+            try:
+                soup = BeautifulSoup(''.join(html),"html.parser")
+                d['content'] = soup.getText()
+                d['title'] = str(soup.title.text)
+
+                cssLinks = soup.findAll('link')
+                # For each image in the document
+                for css in cssLinks:
+                    if not css['href'].startswith('http'):
+                        cssUrl = urljoin(url,css['href'])
+                        filename = "images/"+domain+"/"+urlparse(cssUrl).path
+                    else:
+                        cssUrl = css['href']
+                        filename =  urlparse(css['href']).path
+                    css['href'] = "electronic/"+filename
+                    # If the image is not exist, then download it.
+                    if not os.path.exists(os.path.dirname(filename)):
+                        os.makedirs(os.path.dirname(filename))
+
+                    if not os.path.isfile(filename):
+                        urllib.urlretrieve (cssUrl, filename)
+
+                images = soup.findAll('img')
+                # For each image in the document
+                for img in images:
+                    if not img['src'].startswith('http'):
+                        imageUrl = urljoin(url,img['src'])
+                        filename = "images/"+domain+"/"+urlparse(imageUrl).path
+                    else:
+                        imageUrl = img['src']
+                        filename =  urlparse(img['src']).path
+                    img['src'] = "electronic/"+filename
+                    # If the image is not exist, then download it.
+                    if not os.path.exists(os.path.dirname(filename)):
+                        os.makedirs(os.path.dirname(filename))
+
+                    if not os.path.isfile(filename):
+                        urllib.urlretrieve (imageUrl, filename)
+
+                anchors = soup.findAll('a')
+                # For each anchor
+                for anchor in anchors:
+                    if not anchor['href'].startswith('http'):
+                        anchor['href'] = urljoin(url,anchor['href'])
+
+                d['html'] = soup.prettify()
+            except Exception,e: 
+                d['html'] = html
+                error_doc_number = error_doc_number+1
             if ( "crawl_data" in doc["_source"] and  bool(doc["_source"]["crawl_data"]) ):
             	for v in doc["_source"]["crawl_data"]:
                     if ("model" in v):
                     	if v["model"] in entities:
                             duplicate = False
                             for i in entities[v["model"]]:
-                                if i["_id"]==doc["_id"]:
+                                if i["id"]==doc["_id"]:
                                     duplicate = True
                                     break
                             if duplicate == False:
-                    		    entities[v["model"]].append(doc)
+                    		    entities[v["model"]].append(d)
                     	else:
                             entities[v["model"]] = []
-                            entities[v["model"]].append(doc)
+                            entities[v["model"]].append(d)
 
 entities = sorted(entities.items(), key=lambda x:len(x[1]), reverse=True)
 
@@ -64,11 +130,10 @@ for k,v in entities:
     minTimestamp = 400000000000000
     maxTimestamp = 0
     for doc in v :
-        print doc["_id"]
-        if doc["_source"]["timestamp"] < minTimestamp :
-            minTimestamp = doc["_source"]["timestamp"]
-        if doc["_source"]["timestamp"] > maxTimestamp :
-            maxTimestamp = doc["_source"]["timestamp"]
+        if doc["timestamp"] < minTimestamp :
+            minTimestamp = doc["timestamp"]
+        if doc["timestamp"] > maxTimestamp :
+            maxTimestamp = doc["timestamp"]
     
     print maxTimestamp, ' - ', minTimestamp
     data = {"title": "Crime Warning","docs":v,"entity":k, "duration":maxTimestamp-minTimestamp}
